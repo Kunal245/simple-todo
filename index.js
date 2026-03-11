6977298500c71dd9c6dc84ed7c131cda8a4615a2
Kunal Nimje <kunalnimje245@gmail.com>
2026-03-11 14:30:37 +0530

    Todo backend done

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const { createTodo } = require("./types");
+const { todo } = require("node:test");
 app.use(express.json());
 
-app.post("/todo", function(req, res){
+app.post("/todo", async function(req, res){
     const createPayload = req.body
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.sucess){
@@ -11,13 +12,24 @@ app.post("/todo", function(req, res){
             msg: "You send wrong inputs"
         })
     }
+    //put in mongodb
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: 'Stored todo in db '
+    })
 })
 
-app.get("/todos", function(req, res){
-    
+app.get("/todos", async function(req, res){
+    const todos = await todo.find({});
+    console.log(todos)
 })
 
-app.put("/completed", function(req, res){
+app.put("/completed", async function(req, res){
     const updatePayload = req.body
     const parsedPayload = createTodo.safeParse(updatePayload);
     if (!parsedPayload.sucess){
@@ -25,6 +37,16 @@ app.put("/completed", function(req, res){
             msg: "You send wrong inputs"
         })
     }
+
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed: true
+    })
+
+    res.json({
+        msg: 'Marked as completed'
+    })
 })
 
 app.listen(3000);
