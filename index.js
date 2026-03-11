17a2079047c56911ec1515f45573efdb45dba7d8
Kunal Nimje <kunalnimje245@gmail.com>
2026-03-11 13:52:55 +0530

    Updated HTTP server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const { createTodo } = require("./types");
 app.use(express.json());
 
 app.post("/todo", function(req, res){
-    
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.sucess){
+        res.status(411).json({
+            msg: "You send wrong inputs"
+        })
+    }
 })
 
 app.get("/todos", function(req, res){
@@ -11,7 +18,13 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-    
+    const updatePayload = req.body
+    const parsedPayload = createTodo.safeParse(updatePayload);
+    if (!parsedPayload.sucess){
+        res.status(411).json({
+            msg: "You send wrong inputs"
+        })
+    }
 })
 
 app.listen(3000);
